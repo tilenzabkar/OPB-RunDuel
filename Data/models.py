3f2f74580a3453b18c741aebc59655e985017c30
“tilenzabkar” <tilen.zabkar@gmail.com>
2026-05-06 19:00:01 +0200

    dodan model

---
+++ Data/models.py
@@ -0,0 +1,72 @@
+from dataclasses import dataclass, field
+from dataclasses_json import dataclass_json
+from datetime import datetime
+from enum import Enum
+
+
+class TipIzziva(str, Enum):
+    PET_KM = "5km"
+    DESET_KM = "10km"
+    POL_MARATON = "21km"
+    MARATON = "42km"
+    TEDENSKA_RAZDALJA = "tedenska_razdalja"
+
+
+@dataclass
+class Uporabnik:
+    id: int = field(default=0)
+    uporabnisko_ime: str = field(default="")
+    geslo: str = field(default="")
+    stanje: int = field(default=0)
+
+
+@dataclass_json
+@dataclass
+class UporabnikDto:
+    id: int = field(default=0)
+    uporabnisko_ime: str = field(default="")
+    stanje: int = field(default=0)
+
+
+@dataclass
+class Tek:
+    id: int = field(default=0)
+    datum: datetime = field(default=datetime.now())
+    razdalja: float = field(default=0.0)
+    trajanje: int = field(default=0)
+    uporabnik: int = field(default=0)
+
+
+@dataclass
+class Izziv:
+    id: int = field(default=0)
+    vrsta: TipIzziva = field(default=TipIzziva.TEDENSKA_RAZDALJA)
+    stava: int = field(default=0)
+    datum_zacetka: datetime = field(default=datetime.now())
+    uporabnik_stavi: int = field(default=0)
+    uporabnik_nasprotuje: int = field(default=0)
+    zmagovalec: int = field(default=0)
+
+
+@dataclass_json
+@dataclass
+class IzzivDto:
+    id: int = field(default=0)
+    vrsta: str = field(default="")
+    stava: int = field(default=0)
+    datum_zacetka: datetime = field(default=datetime.now())
+    uporabnik_stavi: int = field(default=0)
+    uporabnik_stavi_ime: str = field(default="")
+    uporabnik_nasprotuje: int = field(default=0)
+    uporabnik_nasprotuje_ime: str = field(default="")
+    zmagovalec: int = field(default=0)
+    zmagovalec_ime: str = field(default="")
+
+
+@dataclass
+class Transakcija:
+    id: int = field(default=0)
+    sprememba: int = field(default=0)
+    cas: datetime = field(default=datetime.now())
+    uporabnik: int = field(default=0)
+    izziv: int = field(default=0)
